a0b7d0f35320928298ffb3440a9b475f60a43932
Nando Rossi <talk@nan.do>
2016-04-18 22:34:04 +0200

    Adds basic functional gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,101 +1,133 @@
-var gulp       = require('gulp'),
-	changed    = require('gulp-changes'),
-	compass    = require('gulp-compass'),
-	concat     = require('gulp-concat'),
-	imagemin   = require('gulp-imagemin'),
-	jshint     = require('gulp-jshint'),
-	notify     = require('gulp-notify'),
-	plumber    = require('gulp-plumber'),
-	stripDebug = require('gulp-strip-debug'),
-	uglify     = require('gulp-uglify'),
-	liveReload = require('gulp-live-reload');
-
-// Gulp plumber error handler
+// Error Handling with Plumber
 var onError = function(err) {
-	console.log(err);
+    console.log(err);
 }
 
-// Lets us type "gulp" on the command line and run all of our tasks
-gulp.task('default', ['copyfiles', 'images', 'jshint', 'scripts', 'styles', 'watch']);
+var gulp 		= require('gulp'),
+	gutil 		= require('gulp-util'),
+	del 		= require('del'), // to delete folders
+	pug 		= require('pug'),
+	gulpPug 	= require('gulp-pug'),
+	sass 		= require('gulp-sass'),
+	browserify 	= require('browserify'), // Does some magic shit I don't understand
+	source 		= require('vinyl-source-stream'), // Rquired for browserify
+	buffer 		= require('vinyl-buffer'), // Rquired for browserify
+	uglify 		= require('gulp-uglify'),
+	imagemin	= require('gulp-imagemin'), // Image minify
+	plumber		= require('gulp-plumber'), // Error Handling
+	notify		= require('gulp-notify'),
+	browserSync	= require('browser-sync').create();
 
-// Copy fonts from a module outside of our project (like Bower)
-gulp.task('copyfiles', function() {
-	gulp.src('./source_directory/**/*.{ttf,woff,eof,svg}')
-	.pipe(gulp.dest('./fonts'));
+// Sets environment variables through gulp-util
+// To invoke: $ gulp --env=prod
+var env = gutil.env.env;
+var sourceDir = 'app';
+var outputDir = '.tmp';
+
+if (env === 'prod') {
+	outputDir = 'dist';
+	console.log("hello");
+}
+if (env === 'dev') {
+}
+
+// Clean output dir first
+gulp.task('clean', function() {
+	return del([
+		outputDir + '/**/*'
+	]);
+});
+
+// Process HTML
+gulp.task('html', function(){
+	return gulp.src(sourceDir + '/**/*.pug')
+		.pipe(plumber({
+			errorHandler: onError
+		}))
+		.pipe(gulpPug({
+			pug: pug,
+			pretty: true
+		}))
+		.pipe(gulp.dest(outputDir))
+		.pipe(notify({ message: 'HTML task complete' }));
+});
+
+// Process scripts
+gulp.task('js', function() {
+	return browserify('./app/scripts/main.js', { debug: env !== 'production' })
+		.bundle()
+		.on('error', function (e) {
+			gutil.log(e);
+		})
+		.pipe(plumber({
+			errorHandler: onError
+		}))
+		.pipe(source('bundle.js'))
+		.pipe(gulp.dest('.tmp/scripts'))
+		.pipe(buffer())
+		.pipe(env === 'prod' ? uglify() : gutil.noop())
+		.pipe(gulp.dest(outputDir + '/scripts'))
+		.pipe(notify({ message: 'JS task complete' }));
+});
+
+// Process styles
+gulp.task('styles', function(){
+	var config = {};
+
+	if (env === 'prod') {
+		config.outputStyle = 'compressed';
+	}
+
+	if (env === 'dev') {
+		config.outputStyle = 'map';
+	}
+
+	return gulp.src(sourceDir + '/styles/**/*.{scss,sass}')
+		.pipe(plumber({
+			errorHandler: onError
+		}))
+		.pipe(sass(config))
+		.pipe(gulp.dest(outputDir + '/styles'))
+		.pipe(browserSync.stream())
+		.pipe(notify({ message: 'Styles task complete' }));
 });
 
 // Compress and minify images to reduce their file size
 gulp.task('images', function() {
-	var imgSrc = './src/images/**/*',
-		imgDst = './images';
+	var imgSrc = sourceDir + '/images/**/*',
+		imgDst = outputDir + '/images';
 
 	return gulp.src(imgSrc)
 		.pipe(plumber({
 			errorHandler: onError
 		}))
-		.pipe(changed(imgDst))
 		.pipe(imagemin())
 		.pipe(gulp.dest(imgDst))
 		.pipe(notify({ message: 'Images task complete' }));
 });
 
-// Hint all of our custom developed Javascript to make sure things are clean
-gulp.task('jshint', function() {
-	return gulp.src('./src/scripts/*.js')
-	.pipe(plumber({
-		errorHandler: onError
-	}))
-	.pipe(jshint())
-	.pipe(jshint.reporter('default'))
-	.pipe(notify({ message: 'JS Hinting task complete' }));
-});
-
-// Combine/Minify/Clean Javascript files
-gulp.task('scripts', function() {
-	return gulp.src('./src/scripts/*.js')
-		.pipe(plumber({
-			errorHandler: onError
-		}))
-		.pipe(concat('app.min.js'))
-		.pipe(stripDebug())
-		.pipe(uglify())
-		.pipe(gulp.dest('./js/'))
-		.pipe(notify({ message: 'Scripts task complete' }));
-});
-
-// Combine and minify Sass/Compass stylesheets
-gulp.task('styles', function() {
-	return gulp.src('./src/sass/*.scss')
-		.pipe(plumber({
-			errorHandler: onError
-		}))
-		.pipe(compass({
-			config_file: './config.rb',
-			css: './css',
-			sass: './sass'
-		}))
-		.pipe(gulp.dest('./css'))
-		.pipe(notify({ message: 'Styles task complete' }));
-});
-
-// This handles watching and running tasks as well as telling our LiveReload server to refresh things
+// Gulp Watch
 gulp.task('watch', function() {
-	// Check for modifications in particular directories
-
-	// Whenever a stylesheet is changed, recompile
-	gulp.watch('./src/sass/**/*.scss', ['styles']);
-
-	// If user-developed Javascript is modified, re-run our hinter and scripts tasks
-	gulp.watch('./src/scripts/**/*.js', ['jshint', 'scripts']);
-
-	// If an image is modified, run our images task to compress images
-	gulp.watch('./src/images/**/*', ['images']);
-
-	// Create a LiveReload server
-	var server = liveReload();
-
-	// Watch any file for a change in the 'src' folder and reload as required
-	gulp.watch(['./src/**']).on('change', function(file) {
-		server.changed(file.path);
-	})
+	gulp.watch(sourceDir + '/**/*.pug', ['html']);
+	gulp.watch(sourceDir + '/scripts/**/*.js', ['js']);
+	gulp.watch(sourceDir + '/styles/**/*.{scss,sass}', ['styles']);
+	gulp.watch(sourceDir + '/**/*.{jpg,png,svg,ico}');
 });
+
+// Development Server
+gulp.task('serve', ['build'], function() {
+	browserSync.init({
+		server: '.tmp'
+	});
+
+	gulp.watch(sourceDir + '/**/*.{jpg,png,svg,ico}', ['images']);
+	gulp.watch(sourceDir + '/styles/**/*.{scss,sass}', ['styles']);
+	gulp.watch(sourceDir + '/scripts/**/*.js', ['js']);
+	gulp.watch(sourceDir + '/**/*.pug', ['html']).on('change', browserSync.reload);
+});
+
+// Build
+gulp.task('build', ['clean', 'html', 'js', 'images', 'styles']);
+
+// Default task
+gulp.task('default', ['serve']);
